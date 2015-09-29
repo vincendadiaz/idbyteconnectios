15e059eb2fcfc08088243acd4b042a460fecde0a
ridwanarvi <ridwanarvi@gmail.com>
2015-09-29 15:12:36 +0700

    the drawer's revival

--- Connected Economy/LeaderboardTabBarController.swift
+++ Connected Economy/LeaderboardTabBarController.swift
@@ -12,9 +12,14 @@ import UIKit
 class LeaderboardTabBarController: UIViewController {
     
     
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
         
     }
     
